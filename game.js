cc077bbc7d90c60437f02ae059e08784d540b7db
sushisuwa <jsuwa@ucsc.edu>
2023-11-24 22:39:30 -0800

    Added another scene and connecting final parts

--- game.js
+++ game.js
@@ -53,7 +53,7 @@ class Demo2 extends AdventureScene {
             .on('pointerover', () => {
                 this.showMessage('Walk down the left path');
             })
-            .on('pointerdown', () => this.gotoScene('Demo3'));
+            .on('pointerdown', () => this.gotoScene('demo4'));
 
         let rightPath = this.add.text(this.w * 0.45, this.w * 0.2, '➡️ Right Path')
         .setFontSize(this.s * 2)
@@ -61,31 +61,90 @@ class Demo2 extends AdventureScene {
             .on('pointerover', () => {
                 this.showMessage('Walk down the right path');
             })
-            .on('pointerdown', () => this.gotoScene('Demo3'));
+            .on('pointerdown', () => this.gotoScene('demo3'));
     }
 }
 
 class Demo3 extends AdventureScene {
     constructor() {
-        super("demo3", "Dungeon Tunnel");
+        super("demo3", "Secret Stash!");
     }
     onEnter() {
-        this.add.text(this.w * 0.3, this.w * 0.4, "just go back")
+        let bucket = this.add.text(this.w * 0.3, this.w * 0.25, "🚰 bucket")
             .setFontSize(this.s * 2)
             .setInteractive()
             .on('pointerover', () => {
-                this.showMessage("Choose wisely..");
+                this.showMessage("This could come in handy later...\nClick to pick up.");
+            })
+            .on('pointerdown', () => {
+                this.gainItem('bucket');
+                this.tweens.add({
+                    targets: bucket,
+                    y: `-=${2 * this.s}`,
+                    alpha: { from: 1, to: 0 },
+                    duration: 500,
+                    onComplete: () => bucket.destroy()
+                });
             })
 
-        let leftPath = this.add.text(this.w * 0.6, this.w * 0.2, '⬅️Left Path')
+        this.add.text(this.w * 0.05, this.w * 0.5,"go back")
+            .setFontSize(this.s * 1)
             .setInteractive()
             .on('pointerover', () => {
-                this.showMessage('Walk down the left path');
+                this.showMessage("This is a dead end...click to go back");
             })
-            .on('pointerdown', () => this.gotoScene('Demo3'));
+            .on('pointerdown', () => this.gotoScene('demo2'));
     }
 }
 
+class Demo4 extends AdventureScene {
+    constructor() {
+        super("demo4", "Witch's Lair");
+    }
+    onEnter() {
+        let witch = this.add.text(this.w * 0.3, this.w * 0.25, "witch")
+            .setFontSize(this.s * 2)
+            .setInteractive()
+            .on('pointerover', () => {
+                this.showMessage("You'll never catch me!");
+                this.tweens.add({
+                    targets: witch,
+                    alpha: {from: 1, to: 0},
+                    duration: 200,
+                    x: this.s + (this.h - 2 * this.s) * Math.random(),
+                    y: this.s + (this.h - 2 * this.s) * Math.random(),
+                    ease: 'Sine.inOut',
+                    duration: 500,
+                    alpha: {from: 0, to: 1},
+                    duration: 400,
+                });
+            })
+            .on('pointerdown', () => {
+                if(this.hasItem('bucket')){
+                    this.showMessage("Ah! I'm melting!!");
+                    this.gotoScene('outro');
+                    this.tweens.add({
+                        targets: witch,
+                        y: `-=${2 * this.s}`,
+                        alpha: { from: 1, to: 0 },
+                        duration: 500,
+                        onComplete: () => witch.destroy()
+                    });
+                }else{
+                    this.showMessage("Better luck next time");
+                    this.gotoScene('bad');
+                }
+            })
+
+        this.add.text(this.w * 0.05, this.w * 0.5,"go back")
+            .setFontSize(this.s * 1)
+            .setInteractive()
+            .on('pointerover', () => {
+                this.showMessage("This is a dead end...click to go back");
+            })
+            .on('pointerdown', () => this.gotoScene('demo2'));
+    }
+}
 
 class Intro extends Phaser.Scene {
     constructor() {
@@ -112,6 +171,17 @@ class Outro extends Phaser.Scene {
     }
 }
 
+class Bad extends Phaser.Scene {
+    constructor() {
+        super('bad');
+    }
+    create() {
+        this.add.text(50, 50, "That's all!").setFontSize(50);
+        this.add.text(50, 100, "Click anywhere to restart.").setFontSize(20);
+        this.input.on('pointerdown', () => this.scene.start('intro'));
+    }
+}
+
 
 const game = new Phaser.Game({
     scale: {
@@ -120,7 +190,7 @@ const game = new Phaser.Game({
         width: 1920,
         height: 1080
     },
-    scene: [Intro, Demo1, Demo2, Outro],
+    scene: [Intro, Demo1, Demo2, Demo3, Demo4, Bad, Outro],
     title: "Adventure Game",
 });
 
